1416aad428157cacfa8a1bdbc098b527eeea6653
yanluizcoito@gmail.com <>
2019-07-31 19:34:17 -0300

    up

--- learning-javascript/digital_innovation_javascript/js/main.js
+++ learning-javascript/digital_innovation_javascript/js/main.js
@@ -1,49 +1,8 @@
-function clicou(){
-    document.getElementById('agradecimento').innerHTML = 'Obrigado por clicar';
-}
-
-function redirecionar(){
-    window.open('https://github.com/yaaanhue');
-    //window.location.href = 'https://github.com/yaaanhue';
-}
-
-function trocar(elemento){
-    //document.getElementById('mousemove').innerHTML = 'Obrigado por passar o mouse'
-    elemento.innerHTML = 'Obrigado por passar o mouse'
-}
-
-function voltar(elemento){
-    //document.getElementById('mousemove').innerHTML = 'Passe o mouse aqui'    //pega o elemento com id mousemove e altera o html
-    elemento.innerHTML = 'Passe o mouse aqui'   //ao colocar this la no html pra passar ele mesmo para a funcao a funcao recebe o elemento e faz a alteracao
-}
-
-function load(){    //onload adicionado como argumento ao body para que execute essa funcao quando o body for carregado
-    alert('pagina carregada');
-}
-
-function funcaoChange(elemento){
-    console.log(elemento.value) //pegar os valores selecionados no onechange select
-}
-
-/*function soma(n1,n2){
+function soma(n1, n2){
     return n1 + n2;
 }
 
-function validaIdade(idade){
-    var validar;
-    if (idade >= 18){
-        validar = true;
-    }else{
-        validar = false;
-    }
-    return validar;
-}
 
-
-var idade = prompt('qual eh sua idade?');
-console.log(validaIdade(idade));
-//alert(soma(10,30))
-*/
 
 
 //var nome = 'Yan Luiz Coito';
