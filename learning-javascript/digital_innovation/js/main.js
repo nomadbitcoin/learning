b8201adaf7aab7eeaf3e5dfbf7a1727f910a6277
yanluizcoito@gmail.com <>
2019-07-31 14:39:03 -0300

    second exercises

--- learning-javascript/digital_innovation/js/main.js
+++ learning-javascript/digital_innovation/js/main.js
@@ -1,4 +1,52 @@
-var nome = 'Yan Luiz Coito';
+function clicou(){
+    document.getElementById('agradecimento').innerHTML = 'Obrigado por clicar';
+}
+
+function redirecionar(){
+    window.open('https://github.com/yaaanhue');
+    //window.location.href = 'https://github.com/yaaanhue';
+}
+
+function trocar(elemento){
+    //document.getElementById('mousemove').innerHTML = 'Obrigado por passar o mouse'
+    elemento.innerHTML = 'Obrigado por passar o mouse'
+}
+
+function voltar(elemento){
+    //document.getElementById('mousemove').innerHTML = 'Passe o mouse aqui'    //pega o elemento com id mousemove e altera o html
+    elemento.innerHTML = 'Passe o mouse aqui'   //ao colocar this la no html pra passar ele mesmo para a funcao a funcao recebe o elemento e faz a alteracao
+}
+
+function load(){    //onload adicionado como argumento ao body para que execute essa funcao quando o body for carregado
+    alert('pagina carregada');
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+/*function soma(n1,n2){
+    return n1 + n2;
+}
+
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+
+
+var idade = prompt('qual eh sua idade?');
+console.log(validaIdade(idade));
+//alert(soma(10,30))
+*/
+
+
+//var nome = 'Yan Luiz Coito';
 /*
 var idade = 22;
 var idade2 = 22
@@ -51,6 +99,8 @@ while (count <= 5){ //loop while
 };
 */
 
+/*
 var d = new Date(); //pegar a data
 alert(d.getMonth()+1)   //devolve apenas o mes (mais um pq comeca do 0)
 alert(d.getMinutes())   //devolve apenas o minuto
+*/
